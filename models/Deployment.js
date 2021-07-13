a4536d7e03bcea7fd18cdecc35699a45c3525726
Yehor <photo2mir@gmail.com>
2021-07-13 21:48:19 +0200

    rename release model to deployment

--- models/Deployment.js
+++ models/Deployment.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const releaseSchema = new Schema({
+const deploymentSchema = new Schema({
     projectName: { type: String, required: true },
     commitHash: { type: String, default: null },
     tag: { type: String, default: null },
     branch: { type: String, default: null },
+    env: { type: String, required: true },
     date: { type: Date, default: Date.now }
 })
 
-module.exports = mongoose.model('Release', releaseSchema)
+module.exports = mongoose.model('Deployment', deploymentSchema)
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const Release = require('../models/Release');
+const Deployment = require('../models/Deployment');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-    const releases = await Release.aggregate([{
+    const deployments = await Deployment.aggregate([{
         $group: {
             _id: "$projectName",
             projectName: { $last: "$projectName" },
+            env: { $last: "$env" },
             commitHash: { $last: "$commitHash" },
             tag: { $last: "$tag" },
             branch: { $last: "$branch" },
@@ -15,7 +16,7 @@ router.get('/', async function(req, res, next) {
         }
     }]).sort({ projectName: 1 });
 
-    res.render('index', { title: 'Projects', releases: releases });
+    res.render('index', { title: 'Projects deployments', deployments: deployments });
 });
 
 module.exports = router;
